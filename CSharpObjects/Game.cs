aac7c4423a8e69464bfc2bd1add0720ed3bc05a6
jkchuong <jkchuong@gmail.com>
2020-12-09 16:32:13 +0000

    testing functionality of DistanceTo function

--- CSharpObjects/Game.cs
+++ CSharpObjects/Game.cs
@@ -11,15 +11,17 @@ namespace CSharpObjects
         public static void Main(string[] args)
         {
             Map map = new Map(8, 5);
-            Point point = new Point(10, 1);
+            Point point1 = new Point(10, 1);
+            Point point2 = new Point(4, 4);
 
             int area = map.Width * map.Height;
 
 
             Console.WriteLine($"Area: {area}");
-            Console.WriteLine($"Co-ords: ({point.X},{point.Y})");
+            Console.WriteLine($"Co-ord 1: ({point1.X},{point1.Y})");
+            Console.WriteLine($"Co-ord 2: ({point2.X},{point2.Y})");
             
-            if (map.OnMap(point) == true)
+            if (map.OnMap(point1) == true)
             {
                 Console.WriteLine("The point is within the area.");
             }
@@ -27,6 +29,17 @@ namespace CSharpObjects
             {
                 Console.WriteLine("The point is out of bounds");
             }
+
+            if (map.OnMap(point2) == true)
+            {
+                Console.WriteLine("The point is within the area.");
+            }
+            else
+            {
+                Console.WriteLine("The point is out of bounds");
+            }
+
+            Console.WriteLine(point1.DistanceTo(point2));
 
             Console.ReadKey();
         }
