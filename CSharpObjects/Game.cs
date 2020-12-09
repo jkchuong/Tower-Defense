a14cbc49b8b0e26a946e26c933967aed92923f4b
jkchuong <jkchuong@gmail.com>
2020-12-09 11:22:40 +0000

    Add project files.

---
+++ CSharpObjects/Game.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpObjects
+{
+    class Game
+    {
+        public void Main(string[] args)
+        {
+            Tower tower = new Tower();
+        }
+    }
+}
