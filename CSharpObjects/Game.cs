40b18d57e58a2836a644182ebd40babfefcbb797
jkchuong <jkchuong@gmail.com>
2020-12-09 15:03:45 +0000

    Added point class and function to show if point is in bounds

--- CSharpObjects/Game.cs
+++ CSharpObjects/Game.cs
@@ -10,11 +10,23 @@ namespace CSharpObjects
     {
         public static void Main(string[] args)
         {
-            Map map = new Map(5, 7);
+            Map map = new Map(8, 5);
+            Point point = new Point(10, 1);
 
             int area = map.Width * map.Height;
 
-            Console.WriteLine(area);
+
+            Console.WriteLine($"Area: {area}");
+            Console.WriteLine($"Co-ords: ({point.X},{point.Y})");
+            
+            if (map.OnMap(point) == true)
+            {
+                Console.WriteLine("The point is within the area.");
+            }
+            else
+            {
+                Console.WriteLine("The point is out of bounds");
+            }
 
             Console.ReadKey();
         }
