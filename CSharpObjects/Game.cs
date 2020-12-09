adfbe507b059ab6848b787b8da7092447ce7f2d8
jkchuong <jkchuong@gmail.com>
2020-12-09 23:37:08 +0000

    Added exception

--- CSharpObjects/Game.cs
+++ CSharpObjects/Game.cs
@@ -11,26 +11,20 @@ namespace CSharpObjects
         public static void Main(string[] args)
         {
             Map map = new Map(8, 5);
-            MapLocation point1 = new MapLocation(10, 1);
-            MapLocation point2 = new MapLocation(4, 4);
+
+            try
+            {
+                MapLocation point1 = new MapLocation(10, 1, map);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
 
             int area = map.Width * map.Height;
 
 
-            Console.WriteLine($"Area: {area}");
-            Console.WriteLine($"Co-ord 1: ({point1.X},{point1.Y})");
-            Console.WriteLine($"Co-ord 2: ({point2.X},{point2.Y})");
-            
-            if (map.OnMap(point1) == true)
-            {
-                Console.WriteLine("The point is within the area.");
-            }
-            else
-            {
-                Console.WriteLine("The point is out of bounds");
-            }
-
-            Console.WriteLine(point1.DistanceTo(point2));
 
             Console.ReadKey();
         }
