10a6f78a27018e73cb79ce457b589074b952af96
jkchuong <jkchuong@gmail.com>
2020-12-11 11:43:34 +0000

    Added path to the game using array of MapLocations and included method to find each point on path with pathStep

--- CSharpObjects/Game.cs
+++ CSharpObjects/Game.cs
@@ -14,25 +14,52 @@ namespace CSharpObjects
 
             try
             {
-                MapLocation point1 = new MapLocation(10, 1, map);
+                Path path = new Path(
+                    new[]
+                    {
+                        new MapLocation(0, 2, map),
+                        new MapLocation(1, 2, map),
+                        new MapLocation(2, 2, map),
+                        new MapLocation(3, 2, map),
+                        new MapLocation(4, 2, map),
+                        new MapLocation(5, 2, map),
+                        new MapLocation(6, 2, map),
+                        new MapLocation(7, 2, map)
+                    }
+                 );
+
+                MapLocation location = path.GetLocationAt(8);
+
+                if (location != null)
+                {
+                    Console.WriteLine(location.X + "," + location.Y);
+                }
+
+
             }
             catch(OutOfBoundsException ex)
             {
                 Console.WriteLine(ex);
             }
-            catch(TowerDefenseException ex)
+            catch(TowerDefenseException)
             {
                 Console.WriteLine("Unhandled TowerDefenseException.");
             }
             catch(Exception ex)
             {
-                Console.WriteLine("Unhandled Exception.");
+                Console.WriteLine("Unhandled Exception: " + ex);
             }
 
 
             int area = map.Width * map.Height;
 
+            string[] favouriteThings = new string[3];
 
+            Console.WriteLine(favouriteThings);
+
+            favouriteThings[1] = "sunshine";
+
+            Console.WriteLine(favouriteThings[1]);
 
             Console.ReadKey();
         }
--- CSharpObjects/Path.cs
+++ CSharpObjects/Path.cs
@@ -8,6 +8,16 @@ namespace CSharpObjects
 {
     class Path
     {
+        private readonly MapLocation[] _path;
 
+        public Path(MapLocation[] path)
+        {
+            _path = path;
+        }
+
+        public MapLocation GetLocationAt(int pathStep)
+        {
+            return (pathStep < _path.Length) ? _path[pathStep] : null;
+        }
     }
 }
